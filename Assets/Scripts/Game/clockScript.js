153a365919c67c09258b5ef7d9083b9ef69dc75c
peterkarlovsek <karlosp@gmail.com>
2013-11-02 08:29:31 +0100

    fsdafasdf

---
+++ Assets/Scripts/Game/clockScript.js
@@ -0,0 +1,36 @@
+﻿
+var isPaused : boolean = false;
+var startTime : float; //(in seconds)
+var timeRemaining : float; //(in seconds)
+
+function Start () {
+	guiText.material.color = Color.black;
+}
+
+function Update () {
+	if (!isPaused)
+	{
+	// make sure the timer is not paused
+		DoCountdown();
+	}
+}
+
+function DoCountdown() {
+}
+
+function PauseClock()
+{
+	isPaused = true;
+}
+function UnpauseClock()
+{
+	isPaused = false;
+}
+
+function ShowTime()
+{
+}
+
+function TimeIsUp()
+{
+}
