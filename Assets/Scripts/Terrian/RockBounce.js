6531e937118a4fff1d07ad0e35bdba9926138b66
peterkarlovsek <karlosp@gmail.com>
2013-11-07 07:57:20 +0100

    Init chest with gold

---
+++ Assets/Scripts/Terrian/RockBounce.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+Debug.Log("start");
+}
+
+function OnCollisionEnter(col : Collision) {
+	if(col.gameObject.tag == "RockMesh") {
+		Debug.Log("yes! hit tray!");
+	}
+	Debug.Log(col.gameObject.tag);
+}
+
+
+function Update () {
+}
