0bc614148cdc6a66beb087fba3ae239ed6b65026
peterkarlovsek <karlosp@gmail.com>
2013-11-12 12:21:49 +0100

    Zmanjšanj teren, popravljena meja od kje do kje se generirajo steklenice

--- Assets/Scripts/Game/GenerateProblem.js
+++ Assets/Scripts/Game/GenerateProblem.js
@@ -13,7 +13,7 @@ function Update () {
 
 
 function GenerateProblem() {
-	Instantiate(bottle, Vector3(Random.Range(85,980), -0.2, Random.Range(910,1095)), Quaternion.identity);
+	Instantiate(bottle, Vector3(Random.Range(830,980), -0.2, Random.Range(910,1095)), Quaternion.identity);
 }
 
 function OnTriggerEnter (other : Collider) {
