bc9de7591a87df9dcce053991482c45f5a233985
Peter Karlovšek <karlosp@gmail.com>
2013-12-03 19:35:07 +0100

    ...

--- Assets/Scripts/Title/TitleGUI.js
+++ Assets/Scripts/Title/TitleGUI.js
@@ -3,15 +3,22 @@ var audioClip : AudioClip;
 
 function OnGUI () {
 	GUI.skin = customSkin;
-	var buttonW:int = 100;
+	var buttonW:int = 200;
 	var buttonH:int = 50;
 	var halfScreenW:float = Screen.width/2;
 	var halfButtonW:float = buttonW/2;
 	
-	if(GUI.Button(Rect(halfScreenW-halfButtonW,560,buttonW,buttonH),"Začni igro!"))
+	if(GUI.Button(Rect(halfScreenW-halfButtonW,360,buttonW,buttonH),"Start"))
 	{
 		
 		Application.LoadLevel("terrian");
+		AudioSource.PlayClipAtPoint(audioClip, this.transform.position);
+	}
+	
+	if(GUI.Button(Rect(halfScreenW-halfButtonW,420,buttonW,buttonH),"Navodila"))
+	{
+		
+		Application.LoadLevel("help");
 		AudioSource.PlayClipAtPoint(audioClip, this.transform.position);
 	}
 }
